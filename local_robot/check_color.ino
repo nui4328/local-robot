002b7a9920c5b0cfa00083ca77c7747a088a628c
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2023-07-19 17:32:47 +0700

    Update check_color.ino

--- local_robot/check_color.ino
+++ local_robot/check_color.ino
@@ -5,24 +5,34 @@ void check_box()
       &&my_tcs('b')>= boxcolor_red[2] - 0.2 && my_tcs('b') <= boxcolor_red[2] + 0.2)
       {
         Serial.println("red");
+        bz(100); delay(100);
       }
-    else if(my_tcs('r') >= boxcolor_green[0] - 0.2 && my_tcs('r') <= boxcolor_green[0] + 0.2 
-      &&my_tcs('g')>= boxcolor_green[1] - 0.2 && my_tcs('g') <= boxcolor_green[1] + 0.2
-      &&my_tcs('b')>= boxcolor_green[2] - 0.2 && my_tcs('b') <= boxcolor_green[2] + 0.2)
+    else if(my_tcs('r') >= boxcolor_green[0] - 0.1 && my_tcs('r') <= boxcolor_green[0] + 0.1 
+      &&my_tcs('g')>= boxcolor_green[1] - 0.1 && my_tcs('g') <= boxcolor_green[1] + 0.1
+      &&my_tcs('b')>= boxcolor_green[2] - 0.15 && my_tcs('b') <= boxcolor_green[2] + 0.15)
       {
         Serial.println("green");
+        bz(100); delay(100);
+        bz(300); delay(100);
       }
     else if(my_tcs('r') >= boxcolor_blue[0] - 0.2 && my_tcs('r') <= boxcolor_blue[0] + 0.2 
       &&my_tcs('g')>= boxcolor_blue[1] - 0.2 && my_tcs('g') <= boxcolor_blue[1] + 0.2
       &&my_tcs('b')>= boxcolor_blue[2] - 0.2 && my_tcs('b') <= boxcolor_blue[2] + 0.2)
       {
         Serial.println("blue");
+        bz(100); 
+        bz(100);delay(100);
+        bz(300); delay(100);
       }
     else if(my_tcs('r') >= boxcolor_yello[0] - 0.2 && my_tcs('r') <= boxcolor_yello[0] + 0.2 
       &&my_tcs('g')>= boxcolor_yello[1] - 0.2 && my_tcs('g') <= boxcolor_yello[1] + 0.2
       &&my_tcs('b')>= boxcolor_yello[2] - 0.2 && my_tcs('b') <= boxcolor_yello[2] + 0.2)
       {
         Serial.println("yello");
+        bz(100); 
+        bz(100);
+        bz(100);delay(100);
+        bz(300); delay(100);
       }
     else
       {
